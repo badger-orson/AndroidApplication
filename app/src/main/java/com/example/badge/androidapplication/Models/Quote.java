0b685ec937ba1f1a0a3a22095e9a357019ca1421
Daniel Wu <wu15002@byui.edu>
2017-11-06 14:42:27 -0700

    Created Models for Quote. ScheduledQuote, and QuoteCategory

---
+++ app/src/main/java/com/example/badge/androidapplication/Models/Quote.java
@@ -0,0 +1,33 @@
+package com.example.badge.androidapplication.Models;
+
+/**
+ * Created by Daniel Wu on 11/2/2017.
+ */
+
+public class Quote {
+    private String quoteText;
+    private String quoteSource;
+    private String imageURL;
+
+    //Default Constructor
+    public Quote() {
+        quoteText = "no text";
+        quoteSource = "unknown";
+        imageURL = null;
+    }
+    //Non-Default Constructor
+    public Quote(String quoteText, String quoteSource, String imageURL) {
+        this.quoteText = quoteText;
+        this.quoteSource = quoteSource;
+        this.imageURL = imageURL;
+    }
+
+    public String getQuoteText() { return quoteText; }
+    public void setQuoteText(String quoteText) { this.quoteText = quoteText; }
+
+    public String getQuoteSource() { return quoteSource; }
+    public void setQuoteSource(String quoteSource) { this.quoteSource = quoteSource; }
+
+    public String getImageURL() { return imageURL; }
+    public void setImageURL(String imageURL) { this.imageURL = imageURL; }
+}
---
+++ app/src/main/java/com/example/badge/androidapplication/Models/ScheduledQuote.java
@@ -0,0 +1,30 @@
+package com.example.badge.androidapplication.Models;
+
+import java.util.Date;
+
+/**
+ * Created by 1dani on 11/2/2017.
+ */
+
+public class ScheduledQuote {
+    private Date time;
+    private QuoteCategory category;
+    private boolean muted;
+
+    //Default Constructor
+    public ScheduledQuote() {
+        time = new Date();
+        category = QuoteCategory.Motivation;
+        muted = false;
+    }
+    //Getters & Setters
+    public Date getTime() { return time; }
+    public void setTime(Date time) { this.time = time; }
+
+    public QuoteCategory getCategory() { return category; }
+    public void setCategory(QuoteCategory category) { this.category = category;}
+
+    public boolean isMuted() { return muted; }
+    public void mute() { this.muted = true; }
+    public void unMute() { this.muted = false; }
+}
