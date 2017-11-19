52a13983c6bc89db691682adec419b9c2efeacef
Orson Badger <badgero1234@byui.edu>
2017-11-19 15:25:28 -0700

    made a test commit

---
+++ app/src/main/java/com/example/badge/androidapplication/Models/FireBase.java
@@ -0,0 +1,13 @@
+package com.example.badge.androidapplication.Models;
+
+/**
+ * Created by badge on 11/8/2017.
+ */
+
+public class FireBase {
+
+    public static String getCurrentDBInfo(){
+
+        return "This is some quote by a useless thingymajig......";
+    }
+}
