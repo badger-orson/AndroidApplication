c2a48bc920043b7def84abf0b232767be1b6a645
Stavista <mit13020@byui.edu>
2017-12-04 14:46:35 -0700

    Refactored settings view and named ID on Settings

--- app/src/main/java/com/example/badge/androidapplication/HowOftenActivity.java
+++ app/src/main/java/com/example/badge/androidapplication/HowOftenActivity.java
@@ -11,7 +11,7 @@ public class HowOftenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_how_often);
+        setContentView(R.layout.settings);
     }
 
     public void nextView(View view) {
