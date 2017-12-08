83f328706eb2d32c518ed5778b46c5b92e1c91a8
Orson Badger <badgero1234@byui.edu>
2017-12-08 14:06:22 -0700

    made the rest of the app respond to the correct views.
    Signed-off-by: Orson Badger <badgero1234@byui.edu>

--- app/src/main/java/com/example/badge/androidapplication/Browse.java
+++ app/src/main/java/com/example/badge/androidapplication/Browse.java
@@ -10,6 +10,8 @@ import android.support.v7.app.NotificationCompat;
 import android.util.JsonWriter;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.badge.androidapplication.Controllers.FireBase;
 import com.example.badge.androidapplication.Controllers.QuoteController;
@@ -23,33 +25,74 @@ import java.util.List;
 
 public class Browse extends AppCompatActivity {
 
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public static String EXTRA_MESSAGE = "DisplayQuote";
+    private static final String TAG = "BROWSE ACTIVITY!!!" ;
 
-    //Create storage for the quotes to use. On the next view.
-    private List<Quote> funnyQuotes = new ArrayList<>();
-    private List<Quote> inspirationQuotes = new ArrayList<>();
-    private List<Quote> motivationQuotes = new ArrayList<>();
-    private List<Quote> exerciseQuotes = new ArrayList<>();
-
-    private FireBase fb = new FireBase();
-
-    public static final String EXTRA_MESSAGE = "DisplayQuote";
+    public Button inspiration, fitness, wisdom, life, funny, love;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
 
+        //Declare my variables to be used. These are now assigned to the views buttons.
+        inspiration = (Button) findViewById(R.id.inspiration);
+        fitness = (Button) findViewById(R.id.fitness);
+        wisdom = (Button) findViewById(R.id.wisdom);
+        life = (Button) findViewById(R.id.life);
+        funny = (Button) findViewById(R.id.funny);
+        love = (Button) findViewById(R.id.love);
+
+
     }
 
-    public void nextView(View view) {
+    /**
+     * Determine what button is being pressed after finding that information send it in an extra messsage.
+     */
+    public void determineButtonTopic(View v) {
+
+            switch(v.getId()) {
+                case R.id.inspiration:
+                    Log.d(TAG, "determineButtonTopic: " + "Inspiration!!!!");
+                    EXTRA_MESSAGE = "Inspiration";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                case R.id.fitness:
+                    EXTRA_MESSAGE = "Fitness";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                case R.id.wisdom:
+                    EXTRA_MESSAGE = "Wisdom";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                case R.id.life:
+                     EXTRA_MESSAGE = "Life";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                case R.id.love:
+                    EXTRA_MESSAGE = "Love";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                case R.id.funny:
+                    EXTRA_MESSAGE = "Funny";
+                    displayQuote(EXTRA_MESSAGE);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown button ID");
+            }
+    }
+    public void displayQuote(String extra) {
         try {
-            startActivity(new Intent(this, QuoteDisplay.class));
+            Intent intent = new Intent(this, QuoteDisplay.class);
+
+            intent.putExtra("extra", extra);
+            startActivity(intent);
         }
         catch (Exception e) {
             Log.d("Main Activity TRANSFER", e.toString());
         }
     }
+
 
     public void settings(View view) {
         try {
@@ -60,45 +103,47 @@ public class Browse extends AppCompatActivity {
         }
     }
 
-    public void displayQuote(View view) {
-        try {
-            Intent intent = new Intent(this, QuoteDisplay.class);
-            Quote quote = QuoteController.getRandomQuote(QuoteCategory.Funny);
-            Log.d("Quote toString()", "HERE\'S A QUOTE: " + quote.getQuoteText());
 
-            //Serialize Quote to JSON
-            Gson gson = new Gson();
-            String quoteJson = gson.toJson(quote, Quote.class);
-            intent.putExtra(EXTRA_MESSAGE, quoteJson);
 
-            startActivity(intent);
-        }
-        catch (Exception e) {
-            Log.d("Main Activity TRANSFER", e.toString());
-        }
-    }
+//    public void displayQuote(View view) {
+//        try {
+//            Intent intent = new Intent(this, QuoteDisplay.class);
+//            Quote quote = QuoteController.getRandomQuote(QuoteCategory.Funny);
+//            Log.d("Quote toString()", "HERE\'S A QUOTE: " + quote.getQuoteText());
+//
+//            //Serialize Quote to JSON
+//            Gson gson = new Gson();
+//            String quoteJson = gson.toJson(quote, Quote.class);
+//            intent.putExtra(EXTRA_MESSAGE, quoteJson);
+//
+//            startActivity(intent);
+//        }
+//        catch (Exception e) {
+//            Log.d("Main Activity TRANSFER", e.toString());
+//        }
+//    }
 
-    public void testNotif(View view) {
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        android.support.v4.app.NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.common_full_open_on_phone)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
-
-        Intent intent = new Intent(this, QuoteDisplay.class);
-
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-
-        manager.notify(001, mBuilder.build());
-    }
+//    public void testNotif(View view) {
+//        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        android.support.v4.app.NotificationCompat.Builder mBuilder =
+//                new NotificationCompat.Builder(this)
+//                        .setSmallIcon(R.drawable.common_full_open_on_phone)
+//                        .setContentTitle("My notification")
+//                        .setContentText("Hello World!");
+//
+//        Intent intent = new Intent(this, QuoteDisplay.class);
+//
+//        PendingIntent resultPendingIntent =
+//                PendingIntent.getActivity(
+//                        this,
+//                        0,
+//                        intent,
+//                        PendingIntent.FLAG_UPDATE_CURRENT
+//                );
+//        mBuilder.setContentIntent(resultPendingIntent);
+//
+//        manager.notify(001, mBuilder.build());
+//    }
 
 //    public void Test(View view) {
 //        try {
--- app/src/main/java/com/example/badge/androidapplication/QuoteDisplay.java
+++ app/src/main/java/com/example/badge/androidapplication/QuoteDisplay.java
@@ -3,28 +3,129 @@ package com.example.badge.androidapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.badge.androidapplication.Controllers.FireBase;
 import com.example.badge.androidapplication.Models.Quote;
+import com.example.badge.androidapplication.Models.QuoteCategory;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class QuoteDisplay extends AppCompatActivity {
+
+    private static final String TAG = "Quote Display";
+
+    public TextView quoteV, categoryDisplay;
+    public String categoryNext;
+
+    DatabaseReference mDataBase = FirebaseDatabase.getInstance().getReference();
+    Quote q = new Quote();
+    FireBase fb = new FireBase();
+
+    List<Quote> ql = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_display);
 
-        //Get quote from intent
-        Intent intent = getIntent();
-        Gson gson = new Gson();
-        Quote quote = gson.fromJson(intent.getStringExtra(Browse.EXTRA_MESSAGE), Quote.class);
 
-        //Display quote data in activity
-        TextView body = (TextView) findViewById(R.id.textView2);
-        body.setText(quote.getQuoteText());
+        quoteV = (TextView) findViewById(R.id.quote);
+        categoryDisplay = (TextView) findViewById(R.id.category);
 
-        TextView source = (TextView) findViewById(R.id.textView16);
-        source.setText(quote.getQuoteSource());
+
+
+        Bundle bundle = getIntent().getExtras();
+        String extra = bundle.getString("extra");
+
+        categoryNext = extra;
+
+        Log.d(TAG, "onCreate: " + extra);
+
+        getQuotes(extra);
+
+    }
+
+    public void nextQuote(View v) {
+
+        getQuotes(categoryNext);
+
+    }
+
+    private void getQuotes(final String category) {
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+        try {
+            //Determine what Category to pull into our ql or quoteList
+            switch (category) {
+                case "Wisdom":
+                    ql = fb.getWisdomQuotes();
+                    break;
+                case "Love":
+                    ql = fb.getLoveQuotes();
+                    break;
+                case "Inspiration":
+                    ql = fb.getInspirationalQuotes();
+                    break;
+                case "Life":
+                    ql = fb.getLifeQuotes();
+                    break;
+                case "Fitness":
+                    ql = fb.getFitnessQuotes();
+                    break;
+                case "Funny":
+                    ql = fb.getFunnyQuotes();
+                    break;
+                default:
+                    try {
+                        throw new Exception("Invalid Category");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }
+
+            mDataBase.child("Categories/" + category + "/Quotes/").addValueEventListener(new ValueEventListener() {
+
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    int max = ql.size();
+                    int min = 0;
+
+                    Random rand = new Random();
+
+                    final int i = rand.nextInt((max - min));
+                    try {
+                        q = ql.get(i);
+                        quoteV.setText(q.getQuoteText() + "\n\n\t\t\t\t\t-" + q.getQuoteSource());
+                        categoryDisplay.setText(category);
+
+                    } catch (Exception e) {
+                        Log.d(TAG, "onDataChange: " + e.toString());
+                    }
+
+                    // Do all of the logic here .... If you are setting some text then do it here.
+                    // So in the browse set a thing that will switch between the quote topics.
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        } catch (Exception e) {
+            Log.d(TAG, "getQuotes: " + e.toString());
+        }
+
+
     }
 }
