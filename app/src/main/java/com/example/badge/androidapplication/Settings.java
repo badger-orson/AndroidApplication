7d4af6fffcd73809c53aad98626f8255903abfd1
Stavista <mit13020@byui.edu>
2017-12-08 18:05:50 -0700

    Got all checkbox logic working and updating firebase from the settings view

--- app/src/main/java/com/example/badge/androidapplication/Settings.java
+++ app/src/main/java/com/example/badge/androidapplication/Settings.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 
 import com.example.badge.androidapplication.Controllers.FireBase;
 import com.example.badge.androidapplication.Models.NotificationFrequency;
@@ -20,9 +21,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
-import static com.example.badge.androidapplication.Models.QuoteCategory.Fitness;
 
 public class Settings extends AppCompatActivity {
 
@@ -42,22 +40,235 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
-        NotificationFrequency nf = new NotificationFrequency();
 
-        QuoteCategory quoteCategory = QuoteCategory.Fitness;
-        nf.frequency = 1;
-        nf.category = quoteCategory;
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        notificationFrequencies.add(nf);
+        appUser = fb.getUser(currentFirebaseUser);
+        getUsers(currentFirebaseUser);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+         NotificationFrequency nf = new NotificationFrequency();
 
 
-        QuoteCategory quoteCategory2 = QuoteCategory.Funny;
-        nf = new NotificationFrequency();
-        nf.frequency = 3;
-        nf.category = quoteCategory2;
+        // Check which checkbox was clicked
+        switch(view.getId()) {
 
-        notificationFrequencies.add(nf);
+            //notificationFrequencies.clear();
 
+            case R.id._d_fitness: //hello
+                QuoteCategory category = QuoteCategory.Fitness;
+                nf = new NotificationFrequency(category, 1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._w_fitness:
+                category = QuoteCategory.Fitness;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_fitness:
+                category = QuoteCategory.Fitness;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._d_funny:
+                category = QuoteCategory.Funny;
+                nf = new NotificationFrequency(category,1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._w_funny:
+                category = QuoteCategory.Funny;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_funny:
+                category = QuoteCategory.Funny;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._d_inspiration:
+                category = QuoteCategory.Inspiration;
+                nf = new NotificationFrequency(category,1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+
+            case R.id._w_inspiration:
+                category = QuoteCategory.Inspiration;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_inspiration:
+                category = QuoteCategory.Inspiration;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._d_life:
+                category = QuoteCategory.Life;
+                nf = new NotificationFrequency(category,1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._w_life:
+                category = QuoteCategory.Life;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_life:
+                category = QuoteCategory.Life;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._d_love:
+                category = QuoteCategory.Love;
+                nf = new NotificationFrequency(category,1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._w_love:
+                category = QuoteCategory.Love;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_love:
+                category = QuoteCategory.Love;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._d_wisdom:
+                category = QuoteCategory.Wisdom;
+                nf = new NotificationFrequency(category,1);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._w_wisdom:
+                category = QuoteCategory.Wisdom;
+                nf = new NotificationFrequency(category,2);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+            case R.id._m_wisdom:
+                category = QuoteCategory.Wisdom;
+                nf = new NotificationFrequency(category,3);
+                if (checked){
+                    notificationFrequencies.add(nf);
+                    break;
+                }
+                else {
+                    notificationFrequencies.remove(nf);
+                    break;
+                }
+
+        }
+
+    }
+
+    public void nextView(View view) {
+        try {
+            startActivity(new Intent(this, Browse.class));
+        } catch (Exception e) {
+            Log.d("Main Activity TRANSFER", e.toString());
+        }
         //Get the instance of the FireBase Current user
         FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -68,132 +279,7 @@ public class Settings extends AppCompatActivity {
         getUsers(currentFirebaseUser);
     }
 
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-//        boolean checked = ((Settings) view).isChecked();
-//
-//        // Check which checkbox was clicked
-//        switch(view.getId()) {
-//            case R.id._d_fitness: //hello
-//                if (checked){
-//                    QuoteCategory category = null;
-//                    category = category.Fitness;
-//                    NotificationFrequency NF = new NotificationFrequency(category,1);
-//
-//
-//                }
-//            case R.id._w_fitness:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_fitness:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._d_funny:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._w_funny:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_funny:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._d_inspiration:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._w_inspiration:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_inspiration:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._d_life:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._w_life:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_life:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._d_love:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._w_love:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_love:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._d_wisdom:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._w_wisdom:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-//            case R.id._m_wisdom:
-//                if (checked){
-//                    // Put some meat on the sandwich
-//                }
-//                else
-//                    break;
-        //}
-    }
 
-    public void nextView(View view) {
-        try {
-            startActivity(new Intent(this, Browse.class));
-        } catch (Exception e) {
-            Log.d("Main Activity TRANSFER", e.toString());
-        }
-    }
 
 
     private void getUsers(final FirebaseUser firebaseUser) {
